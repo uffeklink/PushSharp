cf5a0f731df9fce17959b491e50c408ad0c10059
Uffe Klink <ukp@odendo.com>
2016-02-28 21:14:57 +0100

    no message

---
+++ TestClient/Form1.Designer.cs
@@ -0,0 +1,75 @@
+﻿namespace TestClient
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.butSentNotification = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // butSentNotification
+            // 
+            this.butSentNotification.Location = new System.Drawing.Point(611, 402);
+            this.butSentNotification.Name = "butSentNotification";
+            this.butSentNotification.Size = new System.Drawing.Size(97, 23);
+            this.butSentNotification.TabIndex = 0;
+            this.butSentNotification.Text = "Sent notification";
+            this.butSentNotification.UseVisualStyleBackColor = true;
+            this.butSentNotification.Click += new System.EventHandler(this.butSentNotification_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(140, 208);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(196, 20);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.Text = "Hej Arun, kommer denne frem?";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(768, 465);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.butSentNotification);
+            this.Name = "Form1";
+            this.ShowInTaskbar = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Pushsharp client";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button butSentNotification;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ TestClient/Form1.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Newtonsoft.Json.Linq;
+using PushSharp.Core;
+using PushSharp.Apple;
+
+namespace TestClient
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void butSentNotification_Click(object sender, EventArgs e)
+        {
+
+            SentNotification("Hej Arun, kommer denne frem?", "e7f1cb61 6b33c8ac fa5124ed 85924351 346a554e 15ebb7fc ef03fb28 6d32d6eb");
+        }
+
+
+        private void SentNotification(string text, string deviceToken)
+        {
+            deviceToken = deviceToken.Replace(" ", string.Empty);
+
+            //var config = new ApnsConfiguration("push-cert.pfx", "push-cert-pwd");
+            // var config = new ApnsConfiguration(ApnsConfiguration.ApnsServerEnvironment.Sandbox, "aps_development.cer", "OapT1986!");
+            var config = new ApnsConfiguration(ApnsConfiguration.ApnsServerEnvironment.Sandbox, "aps_1.cer", "OapT1986!");
+            // Create a new broker
+            var broker = new ApnsServiceBroker(config);
+
+            broker.OnNotificationFailed += this.NotificationFailed;
+            broker.OnNotificationSucceeded += this.NotificationSucceeded;
+
+            // Start the broker
+            broker.Start();
+
+            // Queue a notification to send
+            broker.QueueNotification(new ApnsNotification
+            {
+                DeviceToken = deviceToken,
+                Payload = JObject.Parse("{\"aps\":{\"badge\":7}}")
+            });
+
+            // Stop the broker, wait for it to finish   
+            // This isn't done after every message, but after you're
+            // done with the broker
+            broker.Stop();
+        }
+
+        protected void NotificationSucceeded(ApnsNotification a)
+        {
+            MessageBox.Show("Notification Sent!", this.Text, MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        protected void NotificationFailed(ApnsNotification apnNotification, Exception aggregateEx)
+        {
+
+            // See what kind of exception it was to further diagnose
+            if (aggregateEx is ApnsNotificationException)
+            {
+                var apnsEx = aggregateEx as ApnsNotificationException;
+
+                // Deal with the failed notification
+                var n = apnsEx.Notification;
+
+                MessageBox.Show($"Notification Failed: ID={n.Identifier}, Code={apnsEx.ErrorStatusCode}", this.Text, MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+            }
+            else if (aggregateEx is ApnsConnectionException)
+            {
+                // Something failed while connecting (maybe bad cert?)
+                MessageBox.Show("Notification Failed(Bad APNS Connection)!", this.Text, MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            else
+            {
+                MessageBox.Show(aggregateEx.ToString(), this.Text, MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+    }
+}
